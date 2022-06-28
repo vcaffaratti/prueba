73991b547d0c876f8ecc15728c38ff3bcabcafce
vcaffaratti <vania.caffaratti@mercadolibre.com>
2022-06-28 15:54:53 -0300

    prueba de codigo

---
+++ Codigos utiles.py
@@ -0,0 +1,36 @@
+###Codigos utiles
+
+###bash
+#Activar un env
+# lo crea >>>> py -m venv nombreque
+
+### lo activa >>> prueba/Scripts/activate.bat
+
+###ver carpeta
+#pwd
+#cambiar directorio
+#cd
+#ir atras en la jerarquia del directorio
+#cd ..
+#crear una carpeta 
+#mkdir nombre carpeta
+#ver que hay adentro del directorio donde estoy
+#ls
+#para ver archivos ocultos
+#ls -a 
+
+#####git bash
+#git --version
+#git config --global user.name vcaffaratti
+#git config --git config --global user.name vcaffaratti
+#git config --global core.editor "code --wait"     
+#git config --global -e
+#git config --global core.autocrlf true    #si fuera mac iria un true en lugar de imput
+#git init #inicializa un nuevo repositorio en mi directorio. este directorio estara oculto. para verlo debo hacer:
+#ls - a #muestra directorios ocultos
+#code . #abre la carpeta donde me encuentro en vscode
+# git status # me da el status de mi git
+# gitt add archivo1.txt 
+
+#git commit -m "comit inicial" # con esto comprometemos los archivos que estaban agregados, se incluye un comentario entre parentesis
+#rm archivo.txt #esto lo eliminaria
